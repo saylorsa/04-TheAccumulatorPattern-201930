8c940ae18a28100f068e3834193be1e4c26922c4
saylorsa <saylorsa@rose-hulman.edu>
2019-03-12 13:43:27 -0400

    draw_lines, Not Done

--- draw_lines.py
+++ draw_lines.py
@@ -5,6 +5,7 @@ def main():
     window1=rg.RoseWindow(350,400,'Tests 1 and 2 of draw_lines')
 
     draw_lines(window1,50,200,4)
+    draw_lines(window1,150,230,12)
 
 
     window1.render()
@@ -13,8 +14,9 @@ def main():
 
 def draw_lines(window,x1,y1,n):
     for k in range(n):
-        x2=x1+200
-        line=rg.Line(rg.Point(x1,y1),rg.Point(x2,(window.height/window.width)*x2-(k*(x2/n))))
+        x2=x1+75
+        y2=1.25*x2
+        line=rg.Line(rg.Point(x1,y1),rg.Point(x2,y2-(k*(x2/n))))
         line.attach_to(window)
 
 main()
