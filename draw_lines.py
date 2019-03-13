7808688372d50bb55f45be27cc090eef78b020b5
saylorsa <saylorsa@rose-hulman.edu>
2019-03-13 01:54:32 -0400

    draw_lines, Not Done

--- draw_lines.py
+++ draw_lines.py
@@ -16,7 +16,7 @@ def draw_lines(window,x1,y1,n):
     for k in range(n):
         x2=x1+100
         y2=-1*(x2-x1)+y1
-        line=rg.Line(rg.Point(x1,y1),rg.Point(x2,y2-(k*((y2-(y1-100))/(n+1)))))
+        line=rg.Line(rg.Point(x1,y1),rg.Point(x2,y2-(k*((y2-y1)/(n+1)))))
         line.attach_to(window)
 
 main()
